a95a02df4f06add175c0f03b70391094c2d6bd06
Mark Sart <marksart.pro@gmail.com>
2020-03-06 03:05:19 +0300

    Паттерн "Наблюдатель": Рефакторинг

--- pkg/behavioral/observer/weatherData.go
+++ pkg/behavioral/observer/weatherData.go
@@ -2,25 +2,21 @@ package observer
 
 import (
 	"fmt"
-	"go-patterns/internal/log"
 	"io"
 	"os"
 	"strconv"
-
-	"github.com/sirupsen/logrus"
 )
 
 // WeatherDater Интерфейс метеостанции
 type WeatherDater interface {
 	SetInput(io.Reader)
 	SetOutput(io.Writer)
-	SetLogOutput(io.Writer)
 	getTemperature() float64
-	SetTemperature()
+	SetTemperature() error
 	getHumidity() float64
-	SetHumidity()
+	SetHumidity() error
 	getPressure() float64
-	SetPressure()
+	SetPressure() error
 	MeasurementsChanged()
 }
 
@@ -28,7 +24,6 @@ type WeatherDater interface {
 type weatherData struct {
 	reader      io.Reader
 	writer      io.Writer
-	logger      *logrus.Logger
 	temperature float64
 	humidity    float64
 	pressure    float64
@@ -44,39 +39,55 @@ func (w *weatherData) SetOutput(writer io.Writer) {
 	w.writer = writer
 }
 
-// SetLogOutput Установить writer для логгера
-func (w *weatherData) SetLogOutput(writer io.Writer) {
-	w.logger.SetOutput(writer)
-}
-
-// GetTemperature Вернуть текущую температуру
+// Вернуть текущую температуру
 func (w *weatherData) getTemperature() float64 {
 	return w.temperature
 }
 
 // SetTemperature Установить температуру
-func (w *weatherData) SetTemperature() {
-	w.temperature = w.input("Введите температуру")
+func (w *weatherData) SetTemperature() error {
+	temperature, err := w.input("Введите температуру")
+	if err != nil {
+		return err
+	}
+
+	w.temperature = temperature
+
+	return nil
 }
 
-// GetHumidity Вернуть текущую влажность
+// Вернуть текущую влажность
 func (w *weatherData) getHumidity() float64 {
 	return w.humidity
 }
 
 // SetHumidity Установить влажность
-func (w *weatherData) SetHumidity() {
-	w.humidity = w.input("Введите влажность")
+func (w *weatherData) SetHumidity() error {
+	humidity, err := w.input("Введите влажность")
+	if err != nil {
+		return err
+	}
+
+	w.humidity = humidity
+
+	return nil
 }
 
-// GetPressure Вернуть текущее давление
+// Вернуть текущее давление
 func (w *weatherData) getPressure() float64 {
 	return w.pressure
 }
 
 // SetPressure Установить давление
-func (w *weatherData) SetPressure() {
-	w.pressure = w.input("Введите давление")
+func (w *weatherData) SetPressure() error {
+	pressure, err := w.input("Введите давление")
+	if err != nil {
+		return err
+	}
+
+	w.pressure = pressure
+
+	return nil
 }
 
 // MeasurementsChanged Вызывается при каждом обновлении показаний датчиков
@@ -85,42 +96,33 @@ func (w *weatherData) MeasurementsChanged() {
 }
 
 // Получить данные
-func (w *weatherData) input(label string) float64 {
+func (w *weatherData) input(label string) (float64, error) {
 	var numberStr string
 	var number float64
 
-input:
 	_, err := fmt.Fprintln(w.writer, label)
 	if err != nil {
-		w.logger.Errorln(err)
-
-		return number
+		return 0, err
 	}
 
 	_, err = fmt.Fscanln(w.reader, &numberStr)
 	if err != nil {
-		w.logger.Errorln(err)
-
-		return number
+		return 0, err
 	}
 
 	number, err = strconv.ParseFloat(numberStr, 64)
 	if err != nil {
-		w.logger.Warnln("Введено некорректное значение")
-
-		goto input
+		return 0, err
 	}
 
-	return number
+	return number, nil
 }
 
 // NewWeatherData Создать weatherData
 func NewWeatherData() WeatherDater {
-	logger := log.NewLogger()
 	wd := new(weatherData)
 	wd.reader = os.Stdin
 	wd.writer = os.Stdout
-	wd.logger = logger
 
 	return wd
 }
