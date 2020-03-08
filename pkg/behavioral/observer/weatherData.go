6513bae0bd37aa1b1ddc3ac4503f1cc75dbc8f1f
Mark Sart <marksart.pro@gmail.com>
2020-03-08 22:52:06 +0300

    Паттерн "Наблюдатель": weatherData

--- pkg/behavioral/observer/weatherData.go
+++ pkg/behavioral/observer/weatherData.go
@@ -5,117 +5,153 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"strings"
 )
 
-// WeatherDater Интерфейс метеостанции
-type WeatherDater interface {
-	SetInput(io.Reader)
-	SetOutput(io.Writer)
-	getTemperature() float64
-	SetTemperature() error
-	getHumidity() float64
-	SetHumidity() error
-	getPressure() float64
-	SetPressure() error
+// Интерфейс получения температуры
+type temperatureGetter interface {
+	GetTemperature() float64
+}
+
+// Интерфейс получения влажности
+type humidityGetter interface {
+	GetHumidity() float64
+}
+
+// Интерфейс получения давления
+type pressureGetter interface {
+	GetPressure() float64
+}
+
+// Интерфейс работы с измерениями
+type measurementsHandler interface {
+	SetMeasurements() error
 	MeasurementsChanged()
 }
 
-// Метеостанция
+// WeatherDater Интерфейс источника метеоданных, способного быть субъектом
+type WeatherDater interface {
+	reader
+	writer
+	temperatureGetter
+	humidityGetter
+	pressureGetter
+	measurementsHandler
+	subject
+}
+
+// Источник метеоданных
 type weatherData struct {
 	reader      io.Reader
 	writer      io.Writer
+	observers   map[observer]struct{}
 	temperature float64
 	humidity    float64
 	pressure    float64
 }
 
-// SetInput Установить reader
-func (w *weatherData) SetInput(reader io.Reader) {
+// SetReader Установить reader
+func (w *weatherData) SetReader(reader io.Reader) {
 	w.reader = reader
 }
 
-// SetOutput Установить writer
-func (w *weatherData) SetOutput(writer io.Writer) {
+// SetWriter Установить writer
+func (w *weatherData) SetWriter(writer io.Writer) {
 	w.writer = writer
 }
 
-// Вернуть текущую температуру
-func (w *weatherData) getTemperature() float64 {
+// GetTemperature Получить текущую температуру
+func (w *weatherData) GetTemperature() float64 {
 	return w.temperature
 }
 
-// SetTemperature Установить температуру
-func (w *weatherData) SetTemperature() error {
-	temperature, err := w.input("Введите температуру")
-	if err != nil {
-		return err
-	}
-
-	w.temperature = temperature
-
-	return nil
-}
-
-// Вернуть текущую влажность
-func (w *weatherData) getHumidity() float64 {
+// GetHumidity Получить текущую влажность
+func (w *weatherData) GetHumidity() float64 {
 	return w.humidity
 }
 
-// SetHumidity Установить влажность
-func (w *weatherData) SetHumidity() error {
-	humidity, err := w.input("Введите влажность")
-	if err != nil {
-		return err
-	}
-
-	w.humidity = humidity
-
-	return nil
-}
-
-// Вернуть текущее давление
-func (w *weatherData) getPressure() float64 {
+// GetPressure Получить текущее давление
+func (w *weatherData) GetPressure() float64 {
 	return w.pressure
 }
 
-// SetPressure Установить давление
-func (w *weatherData) SetPressure() error {
-	pressure, err := w.input("Введите давление")
+// SetMeasurements Задать измерения
+func (w *weatherData) SetMeasurements() error {
+	newMeasurements, err := w.input()
 	if err != nil {
 		return err
 	}
 
-	w.pressure = pressure
+	w.temperature = newMeasurements.temperature
+	w.humidity = newMeasurements.humidity
+	w.pressure = newMeasurements.pressure
+
+	w.MeasurementsChanged()
 
 	return nil
 }
 
 // MeasurementsChanged Вызывается при каждом обновлении показаний датчиков
 func (w *weatherData) MeasurementsChanged() {
-	// Здесь будет реализация
+	w.NotifyObservers()
+}
+
+// RegisterObserver Регистрация нового наблюдателя
+func (w *weatherData) RegisterObserver(newObserver observer) {
+	w.observers[newObserver] = struct{}{}
+}
+
+// RemoveObserver Удалить наблюдателя из списка
+func (w *weatherData) RemoveObserver(removedObserver observer) {
+	delete(w.observers, removedObserver)
+}
+
+// NotifyObservers Оповестить наблюдателей
+func (w *weatherData) NotifyObservers() {
+	newMeasurements := new(measurements)
+	newMeasurements.temperature = w.temperature
+	newMeasurements.humidity = w.humidity
+	newMeasurements.pressure = w.pressure
+
+	for currentObserver := range w.observers {
+		currentObserver.Update(newMeasurements)
+	}
 }
 
 // Получить данные
-func (w *weatherData) input(label string) (float64, error) {
-	var numberStr string
-	var number float64
+func (w *weatherData) input() (*measurements, error) {
+	var input string
+	var numbers [3]float64
 
-	_, err := fmt.Fprintln(w.writer, label)
+	_, err := fmt.Fprintln(w.writer, "Введите новые данные через пробел (temperature humidity pressure):")
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
-	_, err = fmt.Fscanln(w.reader, &numberStr)
+	_, err = fmt.Fscanln(w.reader, &input)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
-	number, err = strconv.ParseFloat(numberStr, 64)
-	if err != nil {
-		return 0, err
+	inputs := strings.Split(input, " ")
+
+	for i, measurement := range inputs {
+		if i > 2 {
+			break
+		}
+
+		numbers[i], err = strconv.ParseFloat(measurement, 64)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	return number, nil
+	newMeasurements := new(measurements)
+	newMeasurements.temperature = numbers[0]
+	newMeasurements.humidity = numbers[1]
+	newMeasurements.pressure = numbers[2]
+
+	return newMeasurements, nil
 }
 
 // NewWeatherData Создать weatherData
@@ -123,6 +159,7 @@ func NewWeatherData() WeatherDater {
 	wd := new(weatherData)
 	wd.reader = os.Stdin
 	wd.writer = os.Stdout
+	wd.observers = make(map[observer]struct{})
 
 	return wd
 }
