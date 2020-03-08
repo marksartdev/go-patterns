27e5a82f4f44136fdf8f8d0a218dc8b06b9def64
Mark Sart <marksart.pro@gmail.com>
2020-03-09 01:31:24 +0300

    Паттерн "Наблюдатель": Тест визуального элемента прогноза

--- pkg/behavioral/observer/observer_test.go
+++ pkg/behavioral/observer/observer_test.go
@@ -2,6 +2,7 @@ package observer
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -72,72 +73,29 @@ func TestNewStatisticsDisplay(t *testing.T) {
 	}
 }
 
-//
-//func TestNewForecastDisplay(t *testing.T) {
-//	data := new(measurements)
-//	data.temperature = 20.0
-//	data.humidity = 60.0
-//	data.pressure = 650.0
-//
-//	rand.Seed(512)
-//
-//	buffer := bytes.NewBuffer(make([]byte, 0))
-//	expected := "Forecast:\n"
-//	expected += fmt.Sprintf("\tTemperature: %.1f\n", getCoefficient()*data.temperature)
-//	expected += fmt.Sprintf("\tHumidity: %.1f\n", getCoefficient()*data.humidity)
-//	expected += fmt.Sprintf("\tPressure: %.1f\n\n", getCoefficient()*data.pressure)
-//
-//	display := NewForecastDisplay()
-//	display.SetOutput(buffer)
-//
-//	display.Update(data)
-//
-//	err := display.Display()
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//	if buffer.String() != expected {
-//		t.Errorf(errStringS, expected, buffer.String())
-//	}
-//}
-//
-//func getCoefficient() float64 {
-//	return 0.7 + rand.Float64()*(1.3-0.7)
-//}
-//
-//func TestCurrentConditionsDisplay_DisplayErr(t *testing.T) {
-//	display := NewCurrentConditionsDisplay()
-//	display.SetOutput(new(badWriter))
-//
-//	err := display.Display()
-//	if err == nil {
-//		t.Error("Ожидалась ошибка при отображении текущего состояния")
-//	} else if err != os.ErrInvalid {
-//		t.Error(err)
-//	}
-//}
-//
-//func TestStatisticsDisplay_DisplayErr(t *testing.T) {
-//	display := NewStatisticsDisplay()
-//	display.SetOutput(new(badWriter))
-//
-//	err := display.Display()
-//	if err == nil {
-//		t.Error("Ожидалась ошибка при отображении статистики")
-//	} else if err != os.ErrInvalid {
-//		t.Error(err)
-//	}
-//}
-//
-//func TestForecastDisplay_DisplayErr(t *testing.T) {
-//	display := NewForecastDisplay()
-//	display.SetOutput(new(badWriter))
-//
-//	err := display.Display()
-//	if err == nil {
-//		t.Error("Ожидалась ошибка при отображении прогноза")
-//	} else if err != os.ErrInvalid {
-//		t.Error(err)
-//	}
-//}
+func TestNewForecastDisplay(t *testing.T) {
+	wd := NewWeatherData()
+
+	data := new(measurements)
+	data.temperature = 20.0
+	data.humidity = 60.0
+	data.pressure = 650.0
+
+	rand.Seed(512)
+
+	expected := "Forecast:\n"
+	expected += fmt.Sprintf("\tTemperature: %.1f\n", getCoefficient()*data.temperature)
+	expected += fmt.Sprintf("\tHumidity: %.1f\n", getCoefficient()*data.humidity)
+	expected += fmt.Sprintf("\tPressure: %.1f\n", getCoefficient()*data.pressure)
+
+	display := NewForecastDisplay(wd)
+
+	result := display.Update(data)
+	if result != expected {
+		t.Errorf(errStringS, expected, result)
+	}
+}
+
+func getCoefficient() float64 {
+	return 0.7 + rand.Float64()*(1.3-0.7)
+}
