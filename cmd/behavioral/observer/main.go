9d08afd49ccf3e67331312a9b1369758f9595730
Mark Sart <marksart.pro@gmail.com>
2020-03-10 01:01:59 +0300

    Паттерн "Наблюдатель"

--- cmd/behavioral/observer/main.go
+++ cmd/behavioral/observer/main.go
@@ -8,21 +8,21 @@ import (
 func main() {
 	wd := observer.NewWeatherData()
 
-	fmt.Println("Test 1:")
+	fmt.Println("Update 1:")
 	currentConditionalsDisplay := observer.NewCurrentConditionsDisplay(wd)
 	statisticsDisplay := observer.NewStatisticsDisplay(wd)
 	forecastDisplay := observer.NewForecastDisplay(wd)
 	fmt.Println(wd.SetMeasurements(20, 60, 560))
 
-	fmt.Println("Test 2:")
+	fmt.Println("Update 2:")
 	wd.RemoveObserver(forecastDisplay)
 	fmt.Println(wd.SetMeasurements(10, 50, 620))
 
-	fmt.Println("Test 3:")
+	fmt.Println("Update 3:")
 	wd.RemoveObserver(statisticsDisplay)
 	fmt.Println(wd.SetMeasurements(30, 80, 700))
 
-	fmt.Println("Test 4:")
+	fmt.Println("Update 4:")
 	wd.RemoveObserver(currentConditionalsDisplay)
 	fmt.Println(wd.SetMeasurements(0, 40, 680))
 }
