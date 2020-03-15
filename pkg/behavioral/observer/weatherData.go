e847985de68d8630615e5bc69110f5e45e2a84ae
Mark Sart <marksart.pro@gmail.com>
2020-03-16 00:05:58 +0300

    Паттерн "Наблюдатель": Доработана реализация субъекта

--- pkg/behavioral/observer/weatherData.go
+++ pkg/behavioral/observer/weatherData.go
@@ -14,30 +14,16 @@ type WeatherDater interface {
 
 // Источник метеоданных
 type weatherData struct {
+	changed     bool
 	observers   map[observer]struct{}
 	temperature float64
 	humidity    float64
 	pressure    float64
 }
 
-// GetTemperature Получить текущую температуру
-func (w *weatherData) GetTemperature() float64 {
-	return w.temperature
-}
-
-// GetHumidity Получить текущую влажность
-func (w *weatherData) GetHumidity() float64 {
-	return w.humidity
-}
-
-// GetPressure Получить текущее давление
-func (w *weatherData) GetPressure() float64 {
-	return w.pressure
-}
-
 // MeasurementsChanged Вызывается при каждом обновлении показаний датчиков
 func (w *weatherData) MeasurementsChanged() string {
-	return w.NotifyObservers()
+	return w.NotifyObservers(nil)
 }
 
 // SetMeasurements Задать измерения
@@ -60,19 +46,48 @@ func (w *weatherData) RemoveObserver(removedObserver observer) {
 }
 
 // NotifyObservers Оповестить наблюдателей
-func (w *weatherData) NotifyObservers() string {
+func (w *weatherData) NotifyObservers(newMeasurements *measurements) string {
 	var result string
 
-	newMeasurements := new(measurements)
-	newMeasurements.temperature = w.temperature
-	newMeasurements.humidity = w.humidity
-	newMeasurements.pressure = w.pressure
+	if w.HasChanged() {
+		for currentObserver := range w.observers {
+			result += currentObserver.Update(w, newMeasurements)
+		}
 
-	for currentObserver := range w.observers {
-		result += currentObserver.Update(newMeasurements)
+		w.ClearChanged()
 	}
 
 	return result
+}
+
+// SetChanged Зафиксировать изменения
+func (w *weatherData) SetChanged() {
+	w.changed = true
+}
+
+// HasChanged Проверить фиксацию изменений
+func (w *weatherData) HasChanged() bool {
+	return w.changed
+}
+
+// ClearChanged Снять фиксацию изменений
+func (w *weatherData) ClearChanged() {
+	w.changed = false
+}
+
+// GetTemperature Получить текущую температуру
+func (w *weatherData) GetTemperature() float64 {
+	return w.temperature
+}
+
+// GetHumidity Получить текущую влажность
+func (w *weatherData) GetHumidity() float64 {
+	return w.humidity
+}
+
+// GetPressure Получить текущее давление
+func (w *weatherData) GetPressure() float64 {
+	return w.pressure
 }
 
 // NewWeatherData Создать weatherData
