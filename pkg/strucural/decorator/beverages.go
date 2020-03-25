b5496012803339d474b0c2008c13cc4832c30357
Mark Sart <marksart.pro@gmail.com>
2020-03-25 21:01:11 +0300

    Паттерн "Декоратор": Напитки

--- pkg/strucural/decorator/beverages.go
+++ pkg/strucural/decorator/beverages.go
@@ -10,32 +10,16 @@ type coster interface {
 	Cost() float64
 }
 
-// Интерфейс дополнений
-type addition interface {
-	HasMilk() bool
-	SetMilk()
-	HasSoy() bool
-	SetSoy()
-	HasMocha() bool
-	SetMocha()
-	HasWhip() bool
-	SetWhip()
-}
-
 // Beverage Интерфейс напитка
 type Beverage interface {
 	descriptor
 	coster
-	addition
 }
 
 // Базовая структура напитка
 type beverage struct {
 	description string
-	milk        bool
-	soy         bool
-	mocha       bool
-	whip        bool
+	cost        float64
 }
 
 // GetDescription Получить описание
@@ -43,64 +27,9 @@ func (b *beverage) GetDescription() string {
 	return b.description
 }
 
-// HasMilk Проверить наличие молочной пены
-func (b *beverage) HasMilk() bool {
-	return b.milk
-}
-
-// SetMilk Задать молочную пену
-func (b *beverage) SetMilk() {
-	b.milk = true
-}
-
-// HasSoy Проверить наличие сои
-func (b *beverage) HasSoy() bool {
-	return b.soy
-}
-
-// SetSoy Задать сою
-func (b *beverage) SetSoy() {
-	b.soy = true
-}
-
-// HasMocha Проверить наличие шоколада
-func (b *beverage) HasMocha() bool {
-	return b.mocha
-}
-
-// SetMocha Задать шоколад
-func (b *beverage) SetMocha() {
-	b.mocha = true
-}
-
-// HasWhip Проверить наличие взбитых сливок
-func (b *beverage) HasWhip() bool {
-	return b.whip
-}
-
-// SetWhip Задать взбитые сливки
-func (b *beverage) SetWhip() {
-	b.whip = true
-}
-
 // Cost Рассчитать стоимость
 func (b *beverage) Cost() float64 {
-	cost := 0.0
-
-	if b.HasMilk() {
-		cost += 10.5
-	}
-	if b.HasSoy() {
-		cost += 5.5
-	}
-	if b.HasMocha() {
-		cost += 15.0
-	}
-	if b.HasWhip() {
-		cost += 7.5
-	}
-
-	return cost
+	return b.cost
 }
 
 // Кофе "Домашняя смесь"
@@ -108,15 +37,11 @@ type houseBlend struct {
 	beverage
 }
 
-// Cost Рассчитать стоимость
-func (h *houseBlend) Cost() float64 {
-	return 50.0 + h.beverage.Cost()
-}
-
 // NewHouseBlend Создать кофе "Домашняя смесь"
 func NewHouseBlend() Beverage {
 	bv := new(houseBlend)
 	bv.description = "Кофе \"Домашняя смесь\""
+	bv.cost = .89
 
 	return bv
 }
@@ -126,15 +51,11 @@ type darkRoast struct {
 	beverage
 }
 
-// Cost Рассчитать стоимость
-func (d *darkRoast) Cost() float64 {
-	return 60.0 + d.beverage.Cost()
-}
-
 // NewDarkRoast Создать кофе "Темная обжарка"
 func NewDarkRoast() Beverage {
 	bv := new(darkRoast)
 	bv.description = "Кофе \"Темная обжарка\""
+	bv.cost = .99
 
 	return bv
 }
@@ -144,15 +65,11 @@ type decaf struct {
 	beverage
 }
 
-// Cost Рассчитать стоимость
-func (d *decaf) Cost() float64 {
-	return 40.0 + d.beverage.Cost()
-}
-
 // NewDecaf Создать кофе "Без кофеина"
 func NewDecaf() Beverage {
 	bv := new(decaf)
 	bv.description = "Кофе \"Без кофеина\""
+	bv.cost = 1.05
 
 	return bv
 }
@@ -162,15 +79,11 @@ type espresso struct {
 	beverage
 }
 
-// Cost Рассчитать стоимость
-func (e *espresso) Cost() float64 {
-	return 30.0 + e.beverage.Cost()
-}
-
 // NewEspresso Создать кофе "Эспрессо"
 func NewEspresso() Beverage {
 	bv := new(espresso)
 	bv.description = "Кофе \"Эспрессо\""
+	bv.cost = 1.99
 
 	return bv
 }
