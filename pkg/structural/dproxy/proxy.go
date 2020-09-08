9a02bb70ce200052b21fd8f47866e66982a3288e
Mark Sart <marksart.pro@gmail.com>
2020-09-08 12:03:15 +0300

    Паттерн "Заместитель": Защитный заместитель - заместитель

---
+++ pkg/structural/dproxy/proxy.go
@@ -0,0 +1,103 @@
+package dproxy
+
+import (
+	"runtime"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type proxyPersonBean struct {
+	handler InvocationHandler
+}
+
+// GetName Получить имя.
+func (p proxyPersonBean) GetName() string {
+	resp, err := p.handler.invoke(p.getFuncName())
+	if errHandle(err) {
+		return ""
+	}
+
+	return resp.String()
+}
+
+// GetGender Получить пол.
+func (p proxyPersonBean) GetGender() string {
+	resp, err := p.handler.invoke(p.getFuncName())
+	if errHandle(err) {
+		return ""
+	}
+
+	return resp.String()
+}
+
+// GetInterests Получить интересы.
+func (p proxyPersonBean) GetInterests() string {
+	resp, err := p.handler.invoke(p.getFuncName())
+	if errHandle(err) {
+		return ""
+	}
+
+	return resp.String()
+}
+
+// GetHotOrNotRating Получить рейтинг.
+func (p proxyPersonBean) GetHotOrNotRating() int {
+	resp, err := p.handler.invoke(p.getFuncName())
+	if errHandle(err) {
+		return 0
+	}
+
+	return int(resp.Int())
+}
+
+// SetName Задать имя.
+func (p proxyPersonBean) SetName(name string) {
+	_, err := p.handler.invoke(p.getFuncName(), name)
+	errHandle(err)
+}
+
+// SetGender Задать пол.
+func (p proxyPersonBean) SetGender(gender string) {
+	_, err := p.handler.invoke(p.getFuncName(), gender)
+	errHandle(err)
+}
+
+// SetInterests Задать интересы.
+func (p proxyPersonBean) SetInterests(interests string) {
+	_, err := p.handler.invoke(p.getFuncName(), interests)
+	errHandle(err)
+}
+
+// SetHotOrNotRating Оценить кандидата.
+func (p proxyPersonBean) SetHotOrNotRating(rating int) {
+	_, err := p.handler.invoke(p.getFuncName(), rating)
+	errHandle(err)
+}
+
+func (p proxyPersonBean) getFuncName() string {
+	callers := make([]uintptr, 15)
+	// nolint:gomnd // Пропускаем в стеке вызовы в данном методе
+	n := runtime.Callers(2, callers)
+	frames := runtime.CallersFrames(callers[:n])
+	frame, _ := frames.Next()
+
+	fullFuncName := strings.Split(frame.Function, ".")
+
+	return fullFuncName[len(fullFuncName)-1]
+}
+
+// NewProxyPersonBean Создать заместителя.
+func NewProxyPersonBean(handler InvocationHandler) PersonBean {
+	return proxyPersonBean{handler}
+}
+
+func errHandle(err error) bool {
+	if err != nil {
+		log.Error(err)
+
+		return true
+	}
+
+	return false
+}
