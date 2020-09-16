3c5d79757ed62056157dfdd42870b2ad6c48dcca
Mark Sart <marksart.pro@gmail.com>
2020-09-16 20:52:36 +0300

    MVC: Генератор

---
+++ pkg/composite/mvc/generator.go
@@ -0,0 +1,59 @@
+package mvc
+
+import (
+	"context"
+	"time"
+)
+
+// Интерфейс генератора.
+type generator interface {
+	start()
+	stop()
+	setTempoInBPM(bpm int)
+}
+
+// Генератор ритма.
+type beatGenerator struct {
+	duration  time.Duration
+	beatEvent func()
+	cancel    context.CancelFunc
+	started   bool
+}
+
+// Запустить генератор.
+func (b *beatGenerator) start() {
+	if b.started {
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	b.cancel = cancel
+	b.started = true
+
+	go b.do(ctx)
+}
+
+// Остановить генератор.
+func (b *beatGenerator) stop() {
+	b.cancel()
+	b.started = false
+}
+
+// Установить bpm.
+func (b *beatGenerator) setTempoInBPM(bpm int) {
+	b.duration = time.Minute / time.Duration(bpm)
+}
+
+// Рабочий процесс.
+func (b *beatGenerator) do(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			b.beatEvent()
+
+			time.Sleep(b.duration)
+		}
+	}
+}
